004c9dadbbc65c0ae296c102a814ab548b34d112
Nadja <nadja.terzimehic@ifi.lmu.de>
2018-11-26 09:53:40 +0100

    HF: Tutorial 06 materials

---
+++ tutorials/06-nodejs-auth/AuthApp/app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+
+// Our custom modules:
+
+// Instantiate the web app.
+const app = express();
+
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Now all parsing middleware is there and we can use more advanced middleware.
+
+// TODO initialize passport
+
+// Forwarder if logged in
+app.use('/', [ express.static(path.join(__dirname, 'public')) ]);
+
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// Error handlers
+
+// Development error handler
+// Will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
